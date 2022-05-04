158382458bcd29265ab31d6c560fa2ce6db67cc3
AppleVegas <applevegass@gmail.com>
2022-05-04 22:21:50 +0300

    Move files

--- bg.js
+++
@@ -1,4 +0,0 @@
-var s = document.createElement('script');
-s.textContent = "Notification = {}; Notification.requestPermission = function(cb) { return new Promise(function(resolve, reject) { resolve('granted') });}; Notification.permission = 'granted'; PushManager.prototype.subscribe = function(){ return new Promise(function(resolve, reject) { resolve(new PushSubscription) });}; console.log('Disabling notification requests.'); ";
-(document.head||document.documentElement).appendChild(s);
-s.remove();
